25dc93c37bd712e8f8b0ead6b66c3cc627ac64b0
Andre McGrail <andremcgrail@Andres-MacBook-Pro.local>
2017-11-10 13:47:33 +0000

    Filter init work

---
+++ Core/Interface/AnalyticSystem/DropdownEnumMask.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GraphicTestFramework
+{
+	public class DropdownEnumMask : MonoBehaviour {
+
+		public string _selection;
+		public string _default;
+		public bool predefined;
+		public List<EnumMaskOption> contents = new List<EnumMaskOption>();
+		public GameObject contentPrefab;
+		private ToggleGroup _toggleGrp;
+
+		void Start()
+		{
+			if(predefined)
+			{
+				UpdateList();
+			}
+		}
+
+		void Init (string[] options) 
+		{
+			foreach (string s in options)
+			{
+				EnumMaskOption enumMask = new EnumMaskOption();
+				enumMask.label = s;
+				contents.Add(enumMask);
+			}
+			UpdateList();
+			
+		}
+
+		void UpdateList () 
+		{
+			
+		}
+
+		void SelectOption()
+		{
+
+		}
+
+		[System.Serializable]
+		public class EnumMaskOption
+		{
+			public string label;
+			public bool state;
+		}
+
+	}
+}
---
+++ Core/Interface/AnalyticSystem/FilterSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GraphicsTestFramework
+{
+    // ------------------------------------------------------------------------------------
+    // FilterSystem
+    // - The base for the filter system
+    // - Collects filtered data or pairs of data from SQL
+    // - Sends to the Structure for it do view and compare
+
+    public class FilterSystem : MonoBehaviour
+    {
+        // ------------------------------------------------------------------------------------
+        // Variables
+
+
+
+        // Singleton
+        private static FilterSystem _Instance = null;
+        public static FilterSystem Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                    _Instance = (FilterSystem)FindObjectOfType(typeof(FilterSystem));
+                return _Instance;
+            }
+        }
+	}
+}
