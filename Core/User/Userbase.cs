ee0d99a9a69b87186e18c3859cd39d940c8edb42
Matt Roper <mattr@unity3d.com>
2017-11-27 10:25:28 +0000

    Added encrypter DLL and static encrypting/decripting method

--- Core/User/Userbase.cs
+++ Core/User/Userbase.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using Encrypto;
+using GraphicsTestFramework;
 
 [System.Serializable]
 public class UserInfomation
@@ -12,125 +14,10 @@ public class UserInfomation
 
 public class Userbase : MonoBehaviour
 {
-    const string keys = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
-    private void Start()
+    void Start()
     {
-        EncryptString("Matt");
-
+        string encrypted = Common.EncryptPassword("Test123!£$");
+        Debug.Log(Common.EncryptPassword("Test123!£$"));
+        Debug.Log(Common.DecryptPassword(encrypted));
     }
-
-    internal Dictionary<char, string> EncryptString(string T)
-    {
-        Dictionary<char, string> eArray = new Dictionary<char, string>();
-        string encrypted = "";
-        foreach (char F in T)
-        {
-            eArray.Add(char.Parse(GetNewKey(eArray)), ((byte)F).ToString());
-
-        }
-
-        foreach (KeyValuePair<char, string> F in eArray)
-        {
-            encrypted += F.Key.ToString() + F.Value;
-        }
-        Debug.Log(T + " as encrypted: " +  encrypted);
-        Debug.Log(encrypted + "as decrypted: " + DecryptString(encrypted));
-        return eArray;
-    }
-
-    protected string GetNewKey(Dictionary<char, string> dict)
-    {
-        string availableKeys = "";
-        foreach (char F in keys)
-        {
-            if (!dict.ContainsKey(F))
-            {
-                availableKeys += F.ToString();
-            }
-        }
-        string key = availableKeys[Random.Range(0, availableKeys.Length)].ToString();
-
-        return key;
-    }
-
-    internal string DecryptString(string T)
-    {
-
-        string result = "";
-        string buffer = "";
-
-        //foreach (byte b in System.Text.Encoding.UTF8.GetBytes(T.ToCharArray()))
-        //    UnityEngine.Debug.Log(b.ToString());
-
-        foreach (char F in T)
-        {
-
-            if (char.IsLetter(F))
-            {
-                if (buffer.Length > 0)
-                {
-                    int code = int.Parse(buffer);
-                    //byte[] by = Encoding.ASCII.GetBytes(buffer);
-                    result += System.Convert.ToChar(code).ToString();
-                }
-                buffer = "";
-            }
-            else
-            {
-                buffer += F.ToString();
-            }
-        }
-
-        if (buffer.Length > 0)
-        {
-            int code = int.Parse(buffer);
-            //byte[] by = Encoding.ASCII.GetBytes(buffer);
-            result += System.Convert.ToChar(code).ToString();
-        }
-
-        return result;
-    }
-
-    internal string ToHexString(byte[] hex)
-    {
-        if (hex == null) return null;
-        if (hex.Length == 0) return string.Empty;
-
-        var s = new StringBuilder();
-        foreach (byte b in hex)
-        {
-            s.Append(b.ToString("x2"));
-        }
-        return s.ToString();
-    }
-
-    internal byte[] ToHexBytes(string hex)
-    {
-        if (hex == null) return null;
-        if (hex.Length == 0) return new byte[0];
-
-        int l = hex.Length / 2;
-        var b = new byte[l];
-        for (int i = 0; i < l; ++i)
-        {
-            b[i] = System.Convert.ToByte(hex.Substring(i * 2, 2), 16);
-        }
-        return b;
-    }
-
-    internal bool EqualsTo(byte[] bytes, byte[] bytesToCompare)
-    {
-        if (bytes == null && bytesToCompare == null) return true; // ?
-        if (bytes == null || bytesToCompare == null) return false;
-        if (object.ReferenceEquals(bytes, bytesToCompare)) return true;
-
-        if (bytes.Length != bytesToCompare.Length) return false;
-
-        for (int i = 0; i < bytes.Length; ++i)
-        {
-            if (bytes[i] != bytesToCompare[i]) return false;
-        }
-        return true;
-    }
-
 }
